50e620e9cd5754fa79283fa962b6c26aeba13231
Danny Smith <danny.smith@rackspace.co.uk>
2020-07-13 13:50:27 +0100

    fmt

--- examples/main.tf
+++ examples/main.tf
@@ -39,9 +39,9 @@ resource "aws_security_group" "test_sg" {
 module "vpc" {
   source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork//?ref=v0.12.3"
 
-  az_count            = 2
-  cidr_range          = "10.0.0.0/16"
-  name                = "${random_string.rstring.result}-test"
+  az_count   = 2
+  cidr_range = "10.0.0.0/16"
+  name       = "${random_string.rstring.result}-test"
 
   private_cidr_ranges = [
     "10.0.2.0/24",
